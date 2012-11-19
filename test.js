a776df0c1480d81ff48a5a8322ee55d219a2d9ec
Sven Herzberg <herzberg.sven@googlemail.com>
2012-11-19 10:13:03 +0100

    provide a working test case
    This will fix https://travis-ci.org/herzi/posixy/jobs/3254942:
    > Error: ENOENT, cannot link sliff to sloff
    >     at Object.<anonymous> (/home/travis/builds/herzi/posixy/test.js:8:8)
    >     at Module._compile (module.js:446:26)
    >     at Object..js (module.js:464:10)
    >     at Module.load (module.js:353:31)
    >     at Function._load (module.js:311:12)
    >     at Array.0 (module.js:484:10)
    >     at EventEmitter._tickCallback (node.js:190:38)
    
    * test.js: make sure the test works

--- test.js
+++ test.js
@@ -1,12 +1,42 @@
+#!/usr/bin/env node
+
+var child_process = require('child_process');
+var fs = require('fs');
 var posixy;
+
 try {
 	posixy = require('posixy');
 } catch (exception) {
 	posixy = require('./build/Release/posixy');
 }
 
-posixy.link("sliff", "sloff", function (error) {
+// FIXME: turn this shell call into JavaScript code
+child_process.exec("mktemp -d posixy.XXXXXX", function (error, stdout, stderr) {
 	if (error) {
 		throw error;
 	}
+
+	var tmpdir = stdout.match(/^([A-Za-z0-9\.]+)\n$/);
+	if (!tmpdir) {
+		throw new Error('ooops! ' + stdout);
+	}
+	tmpdir = tmpdir[1];
+
+	// FIXME: turn this shell call into JavaScript code
+	child_process.exec("touch " + tmpdir + "/sliff", function (error, stdout, stderr) {
+		if (error) {
+			fs.rmdirSync(tmpdir);
+			throw error;
+		}
+
+		posixy.link(tmpdir + "/sliff", tmpdir + "/sloff", function (error) {
+			fs.unlink(tmpdir + "/sliff");
+			fs.unlink(tmpdir + "/sloff");
+			fs.rmdirSync(tmpdir);
+
+			if (error) {
+				throw error;
+			}
+		});
+	});
 });
