23d15a1f8ee0fb89864049b7a767ff56de843da1
Sven Herzberg <herzberg.sven@googlemail.com>
2012-11-25 23:08:39 +0100

    move the real work into a separate function
    * posixy.cc: the link() call is inside a separate function now

--- posixy.cc
+++ posixy.cc
@@ -11,13 +11,27 @@ struct LinkMarshal
     Persistent<Function>  callback;
     Persistent<String>    from;
     Persistent<String>    to;
+    int                   status;
+    int                   errorNumber;
 };
+
+static void
+LinkHandler (uv_work_t* work)
+{
+    LinkMarshal* marshal = static_cast<LinkMarshal*>(work->data);
+
+    String::Utf8Value from(marshal->from);
+    String::Utf8Value to(marshal->to);
+    marshal->status = link(*from, *to);
+    if (marshal->status) {
+        marshal->errorNumber = errno;
+    }
+}
 
 static Handle<Value>
 Link(const Arguments& args)
 {
     HandleScope scope;
-    int status;
     LinkMarshal* marshal;
 
     Local<Function> callback = Local<Function>::Cast(args[2]);
@@ -26,24 +40,32 @@ Link(const Arguments& args)
     };
 
     marshal = new LinkMarshal;
-    marshal->callback = Persistent<Function>::New(callback);
-    marshal->from = Persistent<String>::New(Local<String>::Cast(args[0]));
-    marshal->to   = Persistent<String>::New(Local<String>::Cast(args[1]));
+    marshal->callback    = Persistent<Function>::New(callback);
+    marshal->from        = Persistent<String>::New(Local<String>::Cast(args[0]));
+    marshal->to          = Persistent<String>::New(Local<String>::Cast(args[1]));
+    marshal->status      = 0;
+    marshal->errorNumber = 0;
 
-    String::Utf8Value from(marshal->from);
-    String::Utf8Value to(marshal->to);
-    status = link(*from, *to);
-    if (status) {
+    uv_work_t* task = new uv_work_t();
+    task->data = marshal;
+
+    LinkHandler(task);
+
+    delete task;
+
+    TryCatch  trap;
+
+    if (marshal->status) {
+        String::Utf8Value from(marshal->from);
+        String::Utf8Value to(marshal->to);
 #define pattern "cannot link %s to %s"
         size_t length = strlen(pattern) + strlen(*from) + strlen(*to) + 1;
         char* buf = (char*)malloc(length);
         size_t written = snprintf(buf, length, pattern, *from, *to);
         assert (written < length);
-        argv[0] = node::ErrnoException(errno, "link()", buf, NULL);
+        argv[0] = node::ErrnoException(marshal->errorNumber, "link()", buf, NULL);
         free(buf);
     }
-
-    TryCatch  trap;
 
     marshal->callback->Call(Context::GetCurrent()->Global(), sizeof(argv)/sizeof(*argv), argv);
     if (trap.HasCaught()) {
